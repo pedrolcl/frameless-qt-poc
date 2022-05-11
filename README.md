8a0459b68d36296f092c744c8e4f65222a5d0828
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2022-05-11 13:23:40 +0200

    updated documentation and license

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # frameless-qt-poc
-FramelessWindow with Qt POC
+
+FramelessWindow Demo for Qt Applications (POC)
+
+## Why this exists?
+
+Since Qt 5.15 there are two new [QWindow](https://doc.qt.io/qt-5/qwindow.html) slots (functions):
+* [startSystemMove](https://doc.qt.io/qt-5/qwindow.html#startSystemMove)
+* [startSystemResize](https://doc.qt.io/qt-5/qwindow.html#startSystemResize)
+
+They can be used to make frameless windows that can be moved and resized like normal windows having
+a frame and a title bar, but the application is totally free to draw those components. This is called 
+Client Side Window Decorations (CSD) and there is a similar QML based demo [here](https://github.com/johanhelsing/qt-csd-demo)
+and also a [blog post](https://www.qt.io/blog/custom-window-decorations) by Johan Helsing.
+
+This POC is not design oriented, but functionality and simplicity. There are many other similar demos
+with much more design oriented, and based on native functions which may be of interest for older 
+Qt versions, for instance:
+* [GoodWindow](https://github.com/antonypro/QGoodWindow)
+* [qt-csd](https://github.com/Longhanks/qt-csd)
+
+This is an easier way, and has the advantage of working on Window, macOS and Linux (both X11/xcb and Wayland).
+Can be used with projects based on Qt 5.15 and Qt 6.x.
 
 ## Results
 
@@ -7,13 +28,13 @@ Tests built with Qt 5.15.2 and Qt 6.3.0
 
 :heavy_check_mark: means that it is usable.
 
-:x: means that it is not usable. See the notes.
+:x: means that it is not usable.
 
 :question: means that the results are not conclusive.
 
-|                  | Qt5 + move window  | Qt5 + resize       | Qt6 + move window  | Qt6 + resize       |
-| ---------------- | ------------------ | ------------------ | ------------------ | ------------------ |
-| Windows          | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: |
-| macOS            | :heavy_check_mark: | :x:                | :heavy_check_mark: | :heavy_check_mark: |
-| Linux X11        | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: |
-| Wayland          | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: |
+| Platform:      | Qt5 + move window  | Qt5 + resize       | Qt6 + move window  | Qt6 + resize       |
+| -------------- | ------------------ | ------------------ | ------------------ | ------------------ |
+| Windows        | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: |
+| macOS          | :heavy_check_mark: | :x:                | :heavy_check_mark: | :heavy_check_mark: |
+| Linux X11/xcb  | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: |
+| Linux Wayland  | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: | :heavy_check_mark: |
--- framelesswindow.cpp
+++ framelesswindow.cpp
@@ -1,3 +1,33 @@
+/*
+    FramelessWindow Demo for Qt Applications
+    Copyright (c) 2022, Pedro López-Cabanillas <plcl@users.sf.net>. All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its
+       contributors may be used to endorse or promote products derived from
+       this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #include <QDebug>
 #include <QFlags>
 #include <QWindow>
@@ -48,10 +78,11 @@ bool FramelessWindow::event(QEvent* ev)
     case QEvent::MouseButtonPress:
         updateCursor(edges);
         if ((m_pseudoCaption != nullptr) && m_pseudoCaption->underMouse()) {
-            windowHandle()->startSystemMove();
+            auto ok = windowHandle()->startSystemMove();
+            qDebug() << "startSystemMove()" << (ok ? "succeeded" : "failed");
         } else if (edges != 0) {
-            windowHandle()->startSystemResize(edges);
-            show();
+            auto ok = windowHandle()->startSystemResize(edges);
+            qDebug() << "startSystemResize()" << (ok ? "succeeded" : "failed");
         }
         break;
     case QEvent::MouseButtonRelease:
@@ -71,9 +102,9 @@ bool FramelessWindow::event(QEvent* ev)
         QHoverEvent *event = static_cast<QHoverEvent*>(ev);
         QPoint p =
     #if QT_VERSION < QT_VERSION_CHECK(6,0,0)
-                    event->pos();
+                   event->pos();
     #else
-                    event->position().toPoint();
+                   event->position().toPoint();
     #endif
         if ( p.x() < margin ) {
             edges |= Qt::LeftEdge;
